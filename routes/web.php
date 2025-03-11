ed00737b4b369c9e7ad5fcfe2a29905424514ad4
chielnotpro <churchiel003@gmail.com>
2025-03-11 13:22:15 +0800

    larido

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/users', [UserController::class, 'index'])->name('user.page.index');
+Route::get('user/{id}', [UserController::class, 'show'])->name('user.page.show');
+Route::get('user/edit', [UserController::class, 'edit'])->name('user.page.edit');
